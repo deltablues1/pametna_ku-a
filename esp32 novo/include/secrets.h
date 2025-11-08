421453278ab155ddcaf736daf4aed70decb39fae
deltablues1 <tgolic555@gmail.com>
2025-11-08 22:04:13 +0100

    Delete esp32 novo directory

--- esp32 novo/include/secrets.h
+++
@@ -1,7 +0,0 @@
-// Copy this file to secrets.h and fill in your credentials. secrets.h is gitignored.
-#define WIFI_SSID "your-ssid"
-#define WIFI_PASSWORD "your-password"
-#define MQTT_HOST "192.168.1.10"
-#define MQTT_USER ""
-#define MQTT_PASS ""
-// These are runtime secrets, not compile-time (loaded in main.cpp)
--- esp32 novo/src/mqtt_if.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <Arduino.h>
-void mqtt_begin();
-void mqtt_loop();
-void mqtt_pub_state_light(int i, bool on);
-void mqtt_pub_state_outlet(int i, bool on);
